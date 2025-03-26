6e8c32e2deab05b6cbb5d35cac2c0d22c23024fa
sandeep25sahu <sandeep.sahu2001@ssipmt.com>
2025-03-27 02:52:17 +0530

    logo

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/Logo1.png";
+import logo from '../../public/logo.png';
 import { FaFacebook, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa6";
 import 'animate.css';
 
